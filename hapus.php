73a6dca5f6cf70196f359e0cd921f80912d3adbd
satriyo23 <137795802+satriyo23@users.noreply.github.com>
2025-07-05 14:55:17 +0700

    Delete hapus.php

--- hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db.php';
-$id = $_GET['id'];
-$conn->query("DELETE FROM mahasiswa WHERE id=$id");
-header("Location: index.php");
-exit(); // It's good practice to add exit() after header()
-?>
